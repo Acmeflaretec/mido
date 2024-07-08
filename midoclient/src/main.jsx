b8ccd432714860498510ac3944e973cda43c51ba
AnanthuManoj <120579729+AnanthuManoj@users.noreply.github.com>
2024-07-08 14:54:24 +0530

    changed browser router to hash router

--- midoclient/src/main.jsx
+++ midoclient/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import theme from './theme';
-import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter>
+   <HashRouter>
     <ThemeProvider theme={theme}>
       <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
