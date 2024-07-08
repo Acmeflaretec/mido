c3c0232b399e0ace55690a43f762794333fe5f4b
AnanthuManoj <120579729+AnanthuManoj@users.noreply.github.com>
2024-07-08 14:34:43 +0530

    changed the font

--- midoclient/src/main.jsx
+++ midoclient/src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import theme from './theme';
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ midoclient/src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Quicksand, sans-serif', // Specify Quicksand as the primary font family
+  },
+});
+
+export default theme;
